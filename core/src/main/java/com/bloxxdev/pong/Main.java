88d94ff463a926d7522b2d2d861a0b71e89c8528
BloxxDev <bloxxydev@gmail.com>
2025-03-25 21:37:20 +0100

    Pong Layout

---
+++ core/src/main/java/com/bloxxdev/pong/Main.java
@@ -0,0 +1,22 @@
+package com.bloxxdev.pong;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/*  */
+public class Main extends ApplicationAdapter {
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
